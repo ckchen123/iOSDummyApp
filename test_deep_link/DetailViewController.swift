8487b2ad8af76de70292ad68d8b8c9dd896087be
ckchen123 <107238964+ckchen123@users.noreply.github.com>
2022-07-08 15:30:32 -0700

    Add files via upload

---
+++ test_deep_link/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  test_deep_link
+//
+//  Created by Cheng-Kai Chen on 6/29/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "DetailVC 1"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
